fd962b347f4e8239ff407c980d9b4bf26f485fa6
Sultan Mahmud Rana <sumahmud@relisource.com>
2022-04-24 00:29:56 +0600

    Bronze League Rank 1464 of 3739

--- spring-challenge-2022.py
+++ spring-challenge-2022.py
@@ -12,13 +12,35 @@ THREAT_FOR_NONE = 0
 THREAT_FOR_MINE = 1
 THREAT_FOR_OPNT = 2
 
+# game const
+BOARD_SIZE_X = 17630
+BOARD_SIZE_Y = 9000
+
 Entity = namedtuple('Entity', [
     'id', 'type', 'x', 'y', 'shield_life', 'is_controlled', 'health', 'vx', 'vy', 'near_base', 'threat_for'
 ])
 
+# game vars
+game_turn = 0
+
 # base_x: The corner of the map representing your base
-base_x, base_y = [int(i) for i in input().split()]
+mybase_x, mybase_y = [int(i) for i in input().split()]
 heroes_per_player = int(input())  # Always 3
+
+opbase_x = BOARD_SIZE_X
+opbase_y = BOARD_SIZE_Y
+myheros_tent = [
+    [100, 50],
+    [100, 100],
+    [100, 150]
+]
+
+if mybase_x != 0:
+    opbase_x = 0
+    opbase_y = 0
+    for i in range(heroes_per_player):
+        myheros_tent[i][0] = mybase_x - myheros_tent[i][0]
+        myheros_tent[i][1] = mybase_y - myheros_tent[i][1]
 
 # game loop
 while True:
@@ -55,34 +77,42 @@ while True:
         else:
             assert False
 
-    threats = []
+    mythreats = []
     for m in monsters:
-        threat_level = 0
-        if m.near_base == 1 and m.threat_for == THREAT_FOR_MINE:
-            threat_level = 1000
-        elif m.threat_for == THREAT_FOR_MINE:
-            threat_level = 500
+        if m.threat_for == THREAT_FOR_MINE:
+            dist_from_base = math.hypot(mybase_x - m.x, mybase_y - m.y)
+            mythreats.append((dist_from_base, m))
+    mythreats.sort()  # sort by distance from base asc
 
-        dist = math.hypot(base_x - m.x, base_y - m.y)
-        threat_level += 500 * (1 / (dist + 1))
-        threats.append((threat_level, m))
+    wind_count = 0
+    control_count = 0
+    shield_count = 0
+    outputs = ['WAIT', 'WAIT', 'WAIT']
+    for i in range(heroes_per_player):
+        if game_turn < 5 or len(mythreats) == 0:
+            outputs[i] = (
+                f'MOVE {myheros_tent[i][0]} {myheros_tent[i][1]}')
+            continue
 
-    threats.sort(reverse=True)
-
-    # debug messages
-    # print(f'T[{len(threats)}]:', end=' ', file=sys.stderr, flush=True)
-    # for t in threats:
-    #     print(f'{t[1].id}', end=',', file=sys.stderr, flush=True)
+        if mythreats:
+            target = mythreats[i % len(mythreats)][1]
+            target_dist = math.hypot(
+                my_heroes[i].x - target.x, my_heroes[i].y - target.y)
+            if wind_count == 0 and target_dist < 1280:
+                outputs[i] = (f'SPELL WIND {opbase_x} {opbase_y}')
+                wind_count += 1
+            # elif control_count == 0 and target_dist >= 1280 and target_dist < 2200:
+            #     outputs[i] = (
+            #         f'SPELL CONTROL {target.id} {target.x} {target.y}')
+            #     control_count += 1
+            # elif shield_count == 0 and target_dist >= 1280 and target_dist < 2200:
+            #     outputs[i] = (f'SPELL SHIELD {target.id}')
+            #     shield_count += 1
+            else:
+                outputs[i] = (f'MOVE {target.x} {target.y}')
+    game_turn += 1
 
     for i in range(heroes_per_player):
         # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-        target = None
-        if threats:
-            target = threats[i % len(threats)][1]
-
         # In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
-        if target:
-            print(f'MOVE {target.x} {target.y}')
-            # print(f'MOVE {target.x} {target.y} {target.id}')
-        else:
-            print('WAIT')
+        print(outputs[i])
